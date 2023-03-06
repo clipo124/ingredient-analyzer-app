6dcfe2e45f0a14c4051764aa6bc3ab6dbb8b1ef2
chris <clipo124@live.kutztown.edu>
2023-03-06 00:30:36 -0500

    Started working on MainActivity.kt and CameraActivity.kt

--- app/src/main/java/com/example/ingredient_analyzer_app/CameraActivity.kt
+++ app/src/main/java/com/example/ingredient_analyzer_app/CameraActivity.kt
@@ -2,10 +2,24 @@ package com.example.ingredient_analyzer_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ingredient_analyzer_app.databinding.ActivityCameraBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class CameraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCameraBinding
+    private lateinit var takePhotoButton: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_camera)
+        takePhotoButton = binding.takePhotoButton
+        takePhotoButton.setOnClickListener {
+            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
+
     }
 }
--- app/src/main/java/com/example/ingredient_analyzer_app/MainActivity.kt
+++ app/src/main/java/com/example/ingredient_analyzer_app/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.ingredient_analyzer_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ingredient_analyzer_app.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var photoButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        photoButton = binding.cameraButton
+        photoButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
