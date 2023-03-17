f8f3bb54f4e64b52038aa4c8ac97031077553427
chris <clipo124@live.kutztown.edu>
2023-03-16 20:33:45 -0400

    Changes made

--- app/src/main/java/com/example/ingredient_analyzer_app/CameraActivity.kt
+++ app/src/main/java/com/example/ingredient_analyzer_app/CameraActivity.kt
@@ -1,25 +1,62 @@
 package com.example.ingredient_analyzer_app
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.ImageCapture
+import androidx.core.app.ActivityCompat
 import com.example.ingredient_analyzer_app.databinding.ActivityCameraBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import java.util.concurrent.ExecutorService
+import android.Manifest
+import androidx.core.content.ContextCompat
+
 
 class CameraActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraBinding
     private lateinit var takePhotoButton: FloatingActionButton
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(R.layout.activity_camera)
-        takePhotoButton = binding.takePhotoButton
-        takePhotoButton.setOnClickListener {
-            Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .show()
+
+        // Request Camera Permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
+
+        takePhotoButton = binding.takePhotoButton
+        takePhotoButton.setOnClickListener {
+        }
+
+    }
+    private fun startCamera() {
+
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
 }
