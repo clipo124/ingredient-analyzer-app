a8e13a987a746595518f48a55eb5e39f64920857
chris <clipo124@live.kutztown.edu>
2023-03-27 17:43:41 -0400

    Added fields for Firebase Firestore Database

--- app/src/main/java/com/kutztown/ingredient_analyzer_app/data/Ingredient.kt
+++ app/src/main/java/com/kutztown/ingredient_analyzer_app/data/Ingredient.kt
@@ -1,5 +1,7 @@
 package com.kutztown.ingredient_analyzer_app.data
 
-data class Ingredient (
-    val name: String
+data class Ingredient(
+    val name: String? = null,
+    val description: String? = null,
+    val tag: Map<String, Boolean>? = null
 )
